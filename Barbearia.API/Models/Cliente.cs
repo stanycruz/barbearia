6e5691d66602edf2c8385ed949429df4777f15bb
Stanislaw Cruz <stanycruz@outlook.com>
2024-06-11 17:45:57 -0300

    criando o cliente

---
+++ Barbearia.API/Models/Cliente.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Barbearia.API.Models
+{
+    public class Cliente
+    {
+        public int ClienteID { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+    }
+}
