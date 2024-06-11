f5131138727407a71e4757310d9123fef2756b54
Stanislaw Cruz <stanycruz@outlook.com>
2024-06-11 18:01:34 -0300

    DataAnnotations serviço

--- Barbearia.API/Models/Servico.cs
+++ Barbearia.API/Models/Servico.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +9,20 @@ namespace Barbearia.API.Models
 {
     public class Servico
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Favor colocar um nome para o serviço")]
+        [DisplayName("Nome Serviço")]
         public string NomeServico { get; set; }
+
+        [DisplayName("Descrição")]
         public string Descricao { get; set; }
+
+        [Range(10, 15, ErrorMessage = "A duração deve ser de 10 até 15 minutos")]
         public int DuracaoMin { get; set; }
+
+        [Range(0.01, 100.00, ErrorMessage = "O preço deve ser entre 0.01 até 100.00")]
         public double Preco { get; set; }
     }
 }
