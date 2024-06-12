5d60f78a4735edf6131da882e58472131b07e3ec
Stanislaw Cruz <stanycruz@outlook.com>
2024-06-12 16:02:01 -0300

    configurando connectionString e DbContext

--- Barbearia.API/Program.cs
+++ Barbearia.API/Program.cs
@@ -1,9 +1,18 @@
+using Barbearia.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string sqlServer = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseSqlServer(sqlServer)
+);
 
 var app = builder.Build();
 
