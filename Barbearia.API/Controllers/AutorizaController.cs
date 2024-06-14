03805b97ee12eb44a1e69794a5bd8e1bf15baa08
Stanislaw Cruz <stanycruz@outlook.com>
2024-06-14 11:41:24 -0300

    criando AutorizaController

---
+++ Barbearia.API/Controllers/AutorizaController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Barbearia.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AutorizaController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        private readonly IConfiguration _config;
+
+        public AutorizaController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, IConfiguration config)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _config = config;
+        }
+    }
+}
